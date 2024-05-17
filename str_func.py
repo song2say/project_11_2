676f7edf7d18c79f89ca5501769fc8f1189d3088
jacob zhuchkov <jacob1989@mail.ru>
2024-05-17 15:27:07 +0300

    add докстринг

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def foo(value: str):
+    """возврат строки в верхнем регистре"""
     return value.upper()
+
+def foo2(value: str):
+    """делает заглавными первый символ каждого слова в строке"""
+    for val in value:
+        output = val.title()
+        print(output)
